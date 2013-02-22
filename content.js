cc5ba4eea3726f09d064459ce7f1bb21dd493957
mingcheng <lucky@gracecode.com>
2013-02-22 15:24:10 +0800

    完成界面弹出框样式和脚本

--- content.js
+++ content.js
@@ -5,10 +5,60 @@
  * @link   http://www.gracecode.com/
  */
 
-var body = document.getElementsByTagName("body")[0];
+~function() {
+    var MIN_WORD_LENGTH = 2, MAX_WORD_LENGTH = 32;
+    var body = document.getElementsByTagName("body")[0],
+        popup = document.getElementById('J_PubMed_Popup'),
+        popupClose = document.getElementById('J_PubMed_PopupClose');
 
-body.addEventListener("mouseup", function() {
-    chrome.extension.sendMessage({ word: "hello" }, function(response) {
-        console.log(response);
+    function trim(s) {
+        return s.replace(/(^\s*)|(\s*$)/g, ""); 
+    }
+
+    function decidePopupPostioin(e) {
+        var left = 0, top = 0, popupWidth = popup.clientWidth;
+
+        if (e.pageX || e.pageY) {
+            left = e.pageX - document.body.scrollLeft;
+            top = e.pageY - document.body.scrollTop;
+        } else {
+            left = e.clientX + document.body.scrollLeft - document.body.clientLeft;
+            top = e.clientY + document.body.scrollTop  - document.body.clientTop;
+        }
+
+        if (popupWidth + left > body.clientWidth) {
+            left = body.clientWidth - popupWidth * 1.5 ;
+        }
+
+        popup.style.left = left + 'px';
+        popup.style.top  = top + 'px';
+    }
+
+    window.addEventListener("scroll", function(e) {
+        if (popup.style.display != 'none') {
+            popup.style.display = 'none';
+        }
     });
-}, false);
+
+    body.addEventListener("mouseup", function(e) {
+       var sText = trim((document.selection == undefined) ? 
+            document.getSelection().toString() : document.selection.createRange().text);
+
+        var pregMatchWord = new RegExp('^\\w{' + MIN_WORD_LENGTH + ',' + MAX_WORD_LENGTH +'}$');
+
+        if (pregMatchWord.test(sText)) {
+            decidePopupPostioin(e);
+            popup.style.display = 'block';
+
+            console.info(sText);
+        } else {
+            popup.style.display = 'none';
+        }
+
+        /*
+        chrome.extension.sendMessage({ word: "hello" }, function(response) {
+            console.log(response);
+        });
+        */
+    }, false);
+}();
