f6b4df37a28d3bc37ddf032aeb7103666cb06181
mingcheng <lucky@gracecode.com>
2013-03-04 12:00:41 +0800

    重构 Cache 逻辑规则

--- background.js
+++ background.js
@@ -5,43 +5,52 @@
  * @link   http://www.gracecode.com/
  */
 
-~function() {
+~function(cscope) {
+    /**
+     * Cacher, cache by localStorage
+     */
+    var Cacher = (function() {
+        var fetchFromCache = function(words) {
+            var keys = '_' + words;
+            return JSON.parse(localStorage.getItem(keys));
+        };
+
+        var saveToCache = function(words, data) {
+            try {
+                var keys = '_' + words;
+                localStorage.setItem(keys, JSON.stringify(data));
+            } catch (e) {
+                if (e == QUOTA_EXCEEDED_ERR) {
+                    //data wasn't successfully saved due to quota exceed so throw an error
+                    console.error('Quota exceeded!'); 
+                }
+            }
+        };
+        
+        return {
+            clear: function() {
+                localStorage.clear();
+            },
+
+            isCached: function(words) {
+                return !!fetchFromCache(words);
+            },
+
+            fetchFromCache: fetchFromCache,
+            saveToCache: saveToCache
+        }
+    })();
 
     function getRequestUrl(word) {
         var baseUrl = 'http://dict.pubmed.cn/webservices/dict/openapi/3223231d'
         return baseUrl + "/" + encodeURIComponent(word) + '?t=' + (+new Date());
     }
 
-    function clearCache() {
-        localStorage.clear();
-    }
-
-    function isCached(words) {
-        return !!fetchFromCache(words);
-    }
-
-    function fetchFromCache(words) {
-        var keys = '_' + words;
-        return JSON.parse(localStorage.getItem(keys));
-    }
-
-    function saveToCache(words, data) {
-        try {
-            var keys = '_' + words;
-            localStorage.setItem(keys, JSON.stringify(data));
-        } catch (e) {
-            if (e == QUOTA_EXCEEDED_ERR) {
-                //data wasn't successfully saved due to quota exceed so throw an error
-                console.error('Quota exceeded!'); 
-            }
-        }
-    }
-
     function fetchTranslate(words, callback)
     {
-        if (isCached(words)) {
+        if (Cacher.isCached(words)) {
             // if cache prisented
-            var data = fetchFromCache(words);
+            var data = Cacher.fetchFromCache(words);
             return callback(data);
         }
 
@@ -55,13 +64,13 @@
                     data: resp
                 };
                 callback(data);
-                saveToCache(words, data);
+                Cacher.saveToCache(words, data);
             } else {
                 callback(null);
             }
         }
 
-        console.info('Send request to: ' + getRequestUrl(words));
+        console.info('[NET] Request from ' + getRequestUrl(words));
         xhr.open('GET', getRequestUrl(words), true);
         xhr.send();	
     }
@@ -75,5 +84,7 @@
         });
     });
 
-    window.fetchTranslate = fetchTranslate;
-}();
+    // Bind to global cscope
+    cscope.fetchTranslate = fetchTranslate;
+    cscope.Cacher = Cacher;
+}(window);
